38605f0a4a91ee43d164322f069118cd59ffbc46
Adam Feldman <feldman.adam.j@gmail.com>
2012-02-03 00:13:37 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Foursquarerailsapp::Application.config.secret_token = '96c7c8b38705e49065180274a8a229044fe7a683f4e7ab37f542092b38b6888c3193f8991eaeef6356aefb789764f2052230df7c9843a7244561744ff04cc244'
